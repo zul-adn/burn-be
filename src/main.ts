5b8ca5118f3d3a5ec036b12a0e679f2f924ae460
zul-adn <zbojetx@gmail.com>
2025-01-02 22:33:13 +0700

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('BURN API DOCUMENTATION')
+    .setDescription('Prodive API route and how to use it')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      in: 'header',
+    })
+    .setVersion('1.0')
+    .addTag('Doc v1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('apidoc', app, document);
+
   await app.listen(3002);
 }
 bootstrap();
